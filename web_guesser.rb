78d928f4befe769269c01b98c77f6fa9b59abf74
PenneyGadget <dreamofgreen@gmail.com>
2015-11-22 12:14:12 -0700

    I4 colorization

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,24 +2,30 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(0..100)
+@color = "background: #0016FF;"
+# @@guesses = 5
 
 get '/' do
-  guess = params['guess']
+  guess = params['guess'].to_i
   message = check_guess(guess)
-  erb :index, :locals => {:secret_number => SECRET_NUMBER, :message => message}
+  erb :index, :locals => {:secret_number => SECRET_NUMBER, :message => message, :color => @color}
 end
 
 def check_guess(guess)
-  guess = guess.to_i
   if guess > SECRET_NUMBER + 5
+    @color = "background: #800000;"
     "Way too high!"
   elsif guess > SECRET_NUMBER
+    @color = "background: #FFE4E1;"
     "Too high!"
   elsif guess < SECRET_NUMBER - 5
+    @color = "background: #800000;"
     "Way too low!"
   elsif guess < SECRET_NUMBER
+    @color = "background: #FFE4E1;"
     "Too low!"
   else
-    "Yay! You got it right!!"
+    @color = "background: #9ACD32;"
+    "Yay! You got it right!! The SECRET NUMBER was #{SECRET_NUMBER}"
   end
 end
